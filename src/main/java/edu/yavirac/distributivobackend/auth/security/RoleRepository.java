d44bbdb8b1873bf04b6d87816032bf85b5fc0f2d
freddyheredia4 <freddyheredia4@gmail.com>
2022-08-23 15:55:47 -0500

    add spring security

---
+++ src/main/java/edu/yavirac/distributivobackend/auth/security/RoleRepository.java
@@ -0,0 +1,11 @@
+package edu.yavirac.distributivobackend.auth.security;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface RoleRepository extends CrudRepository <Role, Long> {
+
+    List<Role> findAll();
+    
+}
---
+++ src/main/java/edu/yavirac/distributivobackend/auth/security/UserRepository.java
@@ -0,0 +1,13 @@
+package edu.yavirac.distributivobackend.auth.security;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository <User, Long>{
+    
+    List<User> findAll();
+
+    User findByUsername(String username);
+    
+}
