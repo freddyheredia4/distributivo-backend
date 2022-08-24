9ac624f25dcaca0281321d562b9356f1fb3338ff
Max <maxitse7@gmail.com>
2022-08-24 08:59:49 -0500

    base de datos

--- src/main/resources/db/database.sql
+++ src/main/resources/db/database.sql
@@ -5,3 +5,10 @@ CREATE TABLE locations(
     name varchar(300),
     coordinates varchar(100)
 );
+--TABLE DOCENTE 
+create table docente(
+	cedula SERIAL NOT NULL,
+	nombre CHARACTER VARYING (100),
+	ingreso date,
+	PRIMARY KEY (cedula)
+)
