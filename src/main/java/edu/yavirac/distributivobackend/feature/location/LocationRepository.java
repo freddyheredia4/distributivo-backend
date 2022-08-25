1465084249621f2dc4179e9df7f830ac70826de0
Anderson Macias <ajo.macias@yavirac.edu.ec>
2022-08-25 09:35:49 -0500

    add logic delete

--- src/main/java/edu/yavirac/distributivobackend/feature/location/LocationRepository.java
+++ src/main/java/edu/yavirac/distributivobackend/feature/location/LocationRepository.java
@@ -1,5 +1,7 @@
 package edu.yavirac.distributivobackend.feature.location;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
@@ -7,9 +9,25 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface LocationRepository extends CrudRepository<Location,Long> {
+public interface LocationRepository extends CrudRepository<Location, Long> {
+
+    final String DELETE = "UPDATE locations SET status=false WHERE id=:id";
+    final String SELECT_ALL = "SELECT * FROM locations WHERE status=true";
+    final String FIND_IGNORE_CASE = "SELECT * FROM locations WHERE name ILIKE 'a%x' AND status=true";
+    final String FIND = "SELECT * FROM locations WHERE id=:id";
+
+    @Query(value = SELECT_ALL)
     List<Location> findAll();
+
+    @Query(value = FIND_IGNORE_CASE)
     List<Location> findByNameLikeIgnoreCase(String term);
 
- 
+    @Query(value = FIND)
+    Optional<Location> findById(@Param("id") Long id);
+
+    @Query(value = DELETE)
+    @Modifying()
+    @Transactional()
+    void deleteLocation(@Param("id") Long id);
+
 }
--- src/main/resources/db/database.sql
+++ src/main/resources/db/database.sql
@@ -1,4 +1,3 @@
-
 --TABLE LOCATION
 CREATE TABLE locations(
     id serial PRIMARY KEY,
@@ -7,3 +6,48 @@ CREATE TABLE locations(
     coordinates varchar(100),
     status BOOLEAN default true
 );
+
+--TABLE CLASSROOM
+CREATE TABLE classrooms(
+    id serial primary key,
+    type integer,
+    location integer,
+    name varchar(100),
+    capacity int,
+    status boolean default true
+);
+
+--TABLE MEANS
+CREATE TABLE means_classrom(
+    id serial PRIMARY KEY,
+    classroom integer,
+    proyect varchar(200),
+    chairs integer,
+    tables integer,
+    status boolean default true
+);
+
+--TABLE TYPES
+CREATE TABLE classroom_types(
+    id serial PRIMARY KEY,
+    code varchar(20),
+    name varchar(100),
+    status boolean default true 
+);
+
+--ADD CONSTRAINTS
+
+--Add location constraint to classroom
+ALTER TABLE classrooms ADD CONSTRAINT
+fk_classroom_location   
+FOREIGN KEY(location) REFERENCES locations(id);
+
+--Add type constraint to classroom
+ALTER TABLE classrooms ADD CONSTRAINT
+fk_classroom_type
+FOREIGN KEY(type) REFERENCES classroom_types(id);
+
+--Add classroom constraint to mean
+ALTER TABLE means_classrom ADD CONSTRAINT
+fk_mean_classroom
+FOREIGN KEY(classroom) REFERENCES classrooms(id);
