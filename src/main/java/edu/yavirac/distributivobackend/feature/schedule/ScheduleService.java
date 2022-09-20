a1261547e6f421d809ec6f5e7c36ab1413062f18
Anderson Macias <ajo.macias@yavirac.edu.ec>
2022-09-20 12:54:39 -0500

    add endpoints schedule

--- src/main/java/edu/yavirac/distributivobackend/feature/schedule/ScheduleService.java
+++ src/main/java/edu/yavirac/distributivobackend/feature/schedule/ScheduleService.java
@@ -1,10 +1,15 @@
 package edu.yavirac.distributivobackend.feature.schedule;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -52,7 +57,7 @@ public class ScheduleService {
             Event currentEvent = new Event();
             if(eventConsult.getHour().equals(hour)){
                 currentEvent.setClassroom(eventConsult.getClassroom());
-                currentEvent.setDay(eventConsult.getDayNumber());
+                currentEvent.setDay(eventConsult.getDate());
                 currentEvent.setHour(eventConsult.getHour());
                 currentEvent.setSubject(eventConsult.getSubject());
                 currentEvent.setGrade(eventConsult.getGrade());
@@ -74,24 +79,96 @@ public class ScheduleService {
 
     public void importExcel(MultipartFile files) throws IOException{
 
+      
+
         try (XSSFWorkbook workbook = new XSSFWorkbook(files.getInputStream())) {
             XSSFSheet worksheet = workbook.getSheetAt(0);
-
-            for (int index = 1; index < worksheet.getPhysicalNumberOfRows(); index++) {
-                if (index > 0) {
-                    TimeConfiguration timeConfiguration = new TimeConfiguration();
-
+     
+            LocalDateTime localDateTime = LocalDateTime.of(2021, 12, 31, 0, 0);
+            
+            localDateTime = localDateTime.plusDays(365);
+            for (int index = 2; index < worksheet.getPhysicalNumberOfRows(); index++) {
+              
+                    
+                    
+                    
                     XSSFRow row = worksheet.getRow(index);
-                    System.out.println(row.getCell(0).getStringCellValue());
-                    //location.setName(row.getCell(0).getStringCellValue());
-                    //location.setDescription(row.getCell(1).getStringCellValue());
-                    //location.setLatitude(Math.round(row.getCell(2).getNumericCellValue()));
-                    //location.setLongitude(Math.round(row.getCell(3).getNumericCellValue()));
+                    if(row == null) break;
+                    saveColumn(row);
+                    
+                   
 
                     
-                }
             }
+           
         }
+    }
+    private void saveColumn(XSSFRow row){
+        Map<String, String> dataValuesMap = new HashMap<>();
+      
+     
+        addValuetoMap("period", dataValuesMap,row.getCell(0));
+        addValuetoMap("career", dataValuesMap,row.getCell(1));
+        addValuetoMap("classroom", dataValuesMap,row.getCell(2));
+        addValuetoMap("date", dataValuesMap,row.getCell(3));
+        addValuetoMap("hour", dataValuesMap,row.getCell(4));
+        addValuetoMap("subject", dataValuesMap,row.getCell(5));
+        //addValuetoMap("ced_doc", dataValuesMap,row.getCell(6));
+        addValuetoMap("teacher", dataValuesMap,row.getCell(7));
+        addValuetoMap("level", dataValuesMap,row.getCell(8));
+        addValuetoMap("parallel", dataValuesMap,row.getCell(9));
+        addValuetoMap("journalist", dataValuesMap,row.getCell(10));
+       TimeConfiguration timeConfiguration = generateTimeConf(dataValuesMap);
+       System.out.println(dataValuesMap);
+       if(timeConfiguration != null)  scheduleRespository.save(timeConfiguration);
+      
+        
+
+
+    }
+
+    private TimeConfiguration generateTimeConf(Map<String, String> map){
+        String grade =  map.get("level")+" " +map
+        .get("journalist").charAt(0) + map
+        .get("parallel")+"-"+map.get("career");
+         Date date = Date.valueOf(map.get("date").replaceAll("/", "-"));
+         
+        String day = date.toLocalDate().getDayOfWeek().toString();
+
+        TimeConfiguration timeConfiguration = new TimeConfiguration();
+        
+        System.out.println(map);
+        System.out.println(day);
+        System.out.println(grade);
+
+        ConsultIds idDis = scheduleRespository
+        .getIdDisByParameters(map
+        .get("teacher"), map.get("period"), map.get("subject"), grade, map
+        .get("hour"), map.get("classroom"), day);
+        System.out.println(idDis);
+         
+
+        timeConfiguration.setClassroom(idDis.getClassroom());
+        timeConfiguration.setDate(Date.valueOf(map.get("date").replaceAll("/", "-")));
+        
+        timeConfiguration.setDay(idDis.getDay());
+        timeConfiguration.setHour(idDis.getHour());
+        timeConfiguration.setOccupiedBy(idDis.getId());
+        timeConfiguration.setSchoolPeriod(idDis.getPeriod());
+        
+ 
+        
+        return timeConfiguration;
+
+
+
+    }
+
+    private void addValuetoMap(String key,Map<String, String> map, XSSFCell cell ){
+        if( cell != null ){
+            map.put(key, cell.getStringCellValue());
+        }
+
     }
     
 }
--- src/main/java/edu/yavirac/distributivobackend/feature/schedule/TimeConfiguration.java
+++ src/main/java/edu/yavirac/distributivobackend/feature/schedule/TimeConfiguration.java
@@ -1,16 +1,21 @@
 package edu.yavirac.distributivobackend.feature.schedule;
 
+import java.sql.Date;
+
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 
 @Data
 public class TimeConfiguration {
+    @Id
     private Long id;
     private Integer schoolPeriod; 
     private Integer classroom;
     private Integer day;
-    private String date;
+    private Date date;
     private Integer hour; 
     private Integer occupiedBy; 
-    private boolean status; 
+    private boolean status = true; 
     private String color;
 }
