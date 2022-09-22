9794485f640f30d9b2df039fdd1671f256afb18d
Max <maxitse7@gmail.com>
2022-09-22 11:48:15 -0500

    search_distributive

---
+++ src/main/java/edu/yavirac/distributivobackend/feature/distributive/DistributiveDTO.java
@@ -0,0 +1,20 @@
+package edu.yavirac.distributivobackend.feature.distributive;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Data;
+
+@Data
+@Table("v_teacher_distributive")
+public class DistributiveDTO{
+    @Id
+    private long id;
+    private String teacherCedula;
+    private String teacher;
+    private String teacherApellido;
+    private String grade;
+    private String subject;
+    private String period;
+}
